6a64e3d95a4d588353ba7fb6ca4fc9d8cc051af7
Juan Manuel Barrios <jmbarrios.ar@outlook.com>
2025-04-21 00:24:01 -0300

    MQTT DHT11 2Fanes

--- Mi_Casa_Inteligente/Arduino_IDE/Test_20250418_ESP32_MQTT_Porton_reconec_wifi_dht11_fan/Test_20250418_ESP32_MQTT_Porton_reconec_wifi_dht11_fan.ino
+++ Mi_Casa_Inteligente/Arduino_IDE/Test_20250418_ESP32_MQTT_Porton_reconec_wifi_dht11_fan/Test_20250418_ESP32_MQTT_Porton_reconec_wifi_dht11_fan.ino
@@ -1,8 +1,9 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <DHT11.h>
+//#include <DHT11.h>
+#include <DHT.h>
 
-#define DHTPIN 35 // Pin donde está conectado el DHT11
+#define DHTPIN 21 // Pin donde está conectado el DHT11
 #define DHTTYPE DHT11  // Tipo de sensor DHT (DHT11 o DHT22)
 
 // ********** Configuración de la red WiFi ******************
@@ -16,7 +17,7 @@ const char* mqttUser = "usermqtt";                   // Usuario (si aplica)
 const char* mqttPassword = "Ia$247";               // Contraseña (si aplica)
 
 // ************ Config pines para relés que activan porton y ventilador ************
-const int porton = 25;  // Conexión a la entrada del modulo relé señal porton
+const int porton = 5;  // Conexión a la entrada del modulo relé señal porton
 const int fan1 = 26;  // Conexión para relé control de Fan1
 const int fan2 = 27;  // Conexión para relé control de Fan2
 //int lastReleState = HIGH; // de inicio el relé esta abierto
@@ -30,9 +31,10 @@ bool fan1State = false;
 bool fan2State = true; // Iniciar con el ventilador 2 encendido (alternado)
 
 // *********Config sensor de temperatura ***************
-int pin=2;
-DHT11 dht11(pin);  // Asignacion del pin del DHT11, el RTC tiene SDA en el A4 (SDA del arduino) y el SCL en el A5 (SCL del arduino)
-const int umbral = 24;  //Temperatura que activa alarma
+//int pin=35;
+//DHT11 dht11(pin);  // Asignacion del pin del DHT11, el RTC tiene SDA en el A4 (SDA del arduino) y el SCL en el A5 (SCL del arduino)
+//const int umbral = 24;  //Temperatura que activa alarma
+DHT dht11(DHTPIN, DHTTYPE);
 
 // ****************** Tópicos MQTT Barrio NORTE **************************
 const char* topicTemp = "casa/garage/dht11/temperatura";     // Tópico para la temperatura
@@ -40,48 +42,17 @@ const char* topicHum = "casa/garage/dht11/humedad";          // Tópico para la 
 
 // Variables de WiFi y MQTT
 WiFiClient espClient;
-PubSubClient mqttClient(espClient);
+PubSubClient client(espClient);
 
 // *********** Configuración inicial ***********
-void setup() {
-  Serial.begin(115200); // Inicializa comunicación serial
-  pinMode(porton, OUTPUT); // Inicializar el pin modulo relé del portón y el led piloto
-  digitalWrite(porton, LOW);  // Apagar inicialmente el relé del portón y el led
-  pinMode(fan1, OUTPUT); // Inicializar el pin modulo relé del ventilador1 y el led piloto
-  pinMode(fan2, OUTPUT); // Inicializar el pin modulo relé del ventilador2 y el led piloto
-  digitalWrite(fan1, LOW);  // Apagar inicialmente el relé del ventilador y el led
-  digitalWrite(fan2, LOW);  // Apagar inicialmente el relé del ventilador y el led
-  connectToWiFi();      // Conexión inicial al WiFi
-  mqttClient.setServer(mqttServer, mqttPort); // Configura el servidor MQTT
-  mqttClient.setCallback(mqttCallback);       // Asigna la función callback
-}
 
-// Bucle principal
-void loop() {
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("WiFi desconectado. Intentando reconectar...");
-    connectToWiFi();
-  }
-
-  if (!mqttClient.connected()) {
-    reconnectMQTT();
-  }
-
-  mqttClient.loop(); // Mantiene la conexión MQTT viva
-  tempyhumd();
-  controlfanes();
-
-  // Opcional: Publicar un mensaje periódicamente
-  /*static unsigned long lastPublish = 0;
-  if (millis() - lastPublish > 5000) {
-    lastPublish = millis();
-    mqttClient.publish("test/topic", "Hello from ESP32!");
-    Serial.println("Mensaje publicado en el topic test/topic.");
-  }*/
-}
+//************* DECLARAR FUNCIONES ***************************
+//void setup_wifi();
+//void callback(char* topic, byte* payload, unsigned int length);
+//void reconnect();
 
 // Función para conectar al WiFi
-void connectToWiFi() {
+void setup_wifi() {
   delay(1 * 30 * 1000); // Espera 30 seg hasta que este disponible el WiFi 247IASbros1
   WiFi.begin(ssid, password);
   Serial.println("Conectando a WiFi...");
@@ -101,23 +72,29 @@ void connectToWiFi() {
 }
 
 // Función para reconectar al servidor MQTT
-void reconnectMQTT() {
-  while (!mqttClient.connected()) {
-    Serial.println("Conectando al servidor MQTT...");
-    if (mqttClient.connect("ESP32Client", mqttUser, mqttPassword)) {
-      Serial.println("Conexión MQTT exitosa!!!");
-      mqttClient.subscribe("casa/porton/pulsar"); // Suscribirse al topico de Barrio Norte
-      Serial.println("Suscrito a topico para control del portón!");
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Intentando conexión MQTT...");
+    // Creamos un cliente ID
+    String clientId = "esp32_";
+    clientId += String(random(0xffff), HEX);
+    // Intentamos conectar
+    if (client.connect(clientId.c_str(),mqttUser,mqttPassword)) {
+      Serial.println("Conectado!");
+      
+      // Suscribirse al tópico del relé del porton
+      client.subscribe("casa/porton/control");  //Barrios NORTE
     } else {
-      Serial.print("Falló la conexión MQTT. Código de error: ");
-      Serial.println(mqttClient.state());
+      Serial.print("Falló la conexión, rc=");
+      Serial.print(client.state());
+      Serial.println(" Reintentando en 2 segundos");
       delay(2000); // Esperar 2 seg antes de intentar nuevamente
     }
   }
 }
 
 // Función callback para manejar mensajes MQTT entrantes
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
+void callback(char* topic, byte* payload, unsigned int length) {
   String messageTemp;
   Serial.print("Mensaje recibido en topic: ");
   Serial.println(topic);
@@ -129,11 +106,13 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
   Serial.println();
 
   // Se compara al tópico suscripto y al payload para activar el relé SI es correcto //
-  if (String(topic) == "casa/porton/pulsar" && messageTemp == "PULSE") {  //Barrio NORTE
+  if (String(topic) == "casa/porton/control" && messageTemp == "PULSO") {  //Barrio NORTE
+    Serial.println("Pulso de 1 segundo enviado al rele");
     digitalWrite(porton, HIGH);  // Activar el relé
-    delay(1000);  // Mantener el estado HIGH durante 1 segundo
+    delay(10000);  // Mantener el estado HIGH durante 1 segundo
     digitalWrite(porton, LOW);   // Desactivar el relay
-    Serial.println("Pulso de 1 segundo enviado al relay");
+    Serial.println("El rele se desactivó");
+    
   }
 }
 
@@ -158,17 +137,17 @@ void tempyhumd() {
   String humStr = String(hi);
 
   // Publicar temperatura
-  mqttClient.publish(topicTemp, tempStr.c_str());
-  Serial.print("Temperatura en Casa: ");
+  client.publish(topicTemp, tempStr.c_str());
+  Serial.print("Temperatura en rack IoT: ");
   Serial.println(tempStr);
 
   // Publicar humedad
-  mqttClient.publish(topicHum, humStr.c_str());
-  Serial.print("Humedad en Casa: ");
+  client.publish(topicHum, humStr.c_str());
+  Serial.print("Humedad en rack IoT: ");
   Serial.println(humStr);
 
-  // Esperar 6 segundos antes de la próxima lectura
-  delay(6000);
+  // Esperar 1 segundo antes de la próxima lectura
+  delay(1000);
 }
 
 //************ CONTROL DE VENTILADORES FAN1 Y FAN2 **************
@@ -192,6 +171,47 @@ void controlfanes() {
   
   delay(1000); // Pequeña pausa para evitar sobrecarga
 }
+
+void setup() {
+  Serial.begin(115200); // Inicializa comunicación serial
+  dht11.begin(); // initialize the DHT11 sensor
+  pinMode(porton, OUTPUT); // Inicializar el pin modulo relé del portón y el led piloto
+  digitalWrite(porton, LOW);  // Apagar inicialmente el relé del portón y el led
+  pinMode(fan1, OUTPUT); // Inicializar el pin modulo relé del ventilador1 y el led piloto
+  pinMode(fan2, OUTPUT); // Inicializar el pin modulo relé del ventilador2 y el led piloto
+  digitalWrite(fan1, LOW);  // Apagar inicialmente el relé del ventilador y el led
+  digitalWrite(fan2, LOW);  // Apagar inicialmente el relé del ventilador y el led
+  
+  setup_wifi();      // Conexión inicial al WiFi
+  client.setServer(mqttServer, mqttPort); // Configura el servidor MQTT
+  client.setCallback(callback);       // Asigna la función callback
+}
+
+// Bucle principal
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi desconectado. Intentando reconectar...");
+    setup_wifi();
+  }
+
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  client.loop(); // Mantiene la conexión MQTT viva
+  tempyhumd();
+  controlfanes();
+
+  // Opcional: Publicar un mensaje periódicamente
+  /*static unsigned long lastPublish = 0;
+  if (millis() - lastPublish > 5000) {
+    lastPublish = millis();
+    client.publish("test/topic", "Hello from ESP32!");
+    Serial.println("Mensaje publicado en el topic test/topic.");
+  }*/
+}
+
+
 
 /*void printFanStates() {
   Serial.println();
